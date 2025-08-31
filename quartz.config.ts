d9728bcd0f84be62dab683f72295cd9ff5eefe8b
Jorg Méndez <jorge.mendez.fredes@gmail.com>
2025-08-30 22:29:09 -0400

    chore: set baseUrl for GitHub Pages and locale es-CL

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Digital Garden",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "es-CL",
+    baseUrl: "https://jorgemendezfredes.github.io/digital-garden",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
